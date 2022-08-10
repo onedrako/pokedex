08b0ee2a0f0870351501e66e262822bb2580174e
onedrako <onedrako@gmail.com>
2022-08-10 14:34:14 -0500

    Fix scrollbar showing where dont have to

--- src/styles/PokemonDetail/PokemonDetail.js
+++ src/styles/PokemonDetail/PokemonDetail.js
@@ -9,8 +9,11 @@ export const PokemonDetailContainer = styled.section`
   box-shadow: rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px;
   border-radius: 10px;
   background-color: #fff;
-  overflow: scroll;
+  overflow: hidden;
   /* position: fixed; */
+  ::-webkit-scrollbar{
+    display: none;
+  }
   
   & > .pokemon-details{
     height: 70%;
